671998ae9af429400b7fae89a7064ba44caf6dd0
Jonas Olsson <jonas.olsson1993@gmail.com>
2016-10-22 10:53:27 +0200

    updated usage example file

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 import minesweeperBoard
 
+# Printing a default-size, random board
 print(minesweeperBoard.generate_board())
+
+# Example: Visualising the board
+customWidth = 15
+customHeight = 20
+mines = 30
+customBoard = minesweeperBoard.generate_board(customWidth, customHeight, mines)
+
+for i in range(0, len(customBoard)):
+    print(customBoard[i], end=' ')
+    if (i+1) % customWidth == 0:
+        print('')
