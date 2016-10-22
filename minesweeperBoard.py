77b7ae902d6861ec1afca86fa5a8c74ae965df21
Jonas Olsson <jonas.olsson1993@gmail.com>
2016-10-22 10:59:38 +0200

    changed order of functions for better readability

--- minesweeperBoard.py
+++ minesweeperBoard.py
@@ -23,6 +23,29 @@ def generate_board(board_width=10, board_height=20, mine_amount=25):
     return squares
 
 
+def get_adjacent_mine_count(square_position, mine_positions, total_squares, board_width):
+    # Only matter if they're not a mine themselves
+    if square_position not in mine_positions:
+        right_above, right_below = (square_position - board_width, square_position + board_width)
+
+        around = [right_above - 1, right_above, right_above + 1, right_below - 1, right_below, right_below + 1,
+                  square_position - 1, square_position + 1]
+
+        adjacent = list(map(lambda x: is_adjacent(x, square_position, total_squares, board_width), around))
+
+        adjacent_mines = 0
+
+        for i in range(0, len(around)):
+            try:
+                adjacent_mines = adjacent_mines + 1 if adjacent[i] and around[i] in mine_positions else adjacent_mines
+            except IndexError as e:
+                pass
+        return adjacent_mines
+    # If they are, indicate m(ine)
+    else:
+        return "m"
+
+
 def is_adjacent(square, middle, total_squares, board_width):
     # Must be inside the board
     if square < 0 or square > total_squares:
@@ -34,35 +57,13 @@ def is_adjacent(square, middle, total_squares, board_width):
     return abs(square_row_position - middle_row_position) < 2
 
 
-def get_adjacent_mine_count(square_position, mine_positions, total_squares, board_width):
-    # Only matter if they're not a mine themselves
-    if square_position not in mine_positions:
-        right_above, right_below = (square_position - board_width, square_position + board_width)
-        
-        around = [right_above - 1, right_above, right_above + 1, right_below - 1, right_below, right_below + 1,
-                  square_position - 1, square_position + 1]
-                    
-        adjacent = list(map(lambda x: is_adjacent(x, square_position, total_squares, board_width), around))
-        
-        adjacent_mines = 0
-        
-        for i in range(0, len(around)):
-            try:
-                adjacent_mines = adjacent_mines + 1 if adjacent[i] and around[i] in mine_positions else adjacent_mines
-            except IndexError as e:
-                pass
-        return adjacent_mines
-    # If they are, indicate m(ine)
-    else:
-        return "m"
-
 if __name__ == "__main__":
     width = 10 if len(sys.argv) < 2 or not sys.argv[1].isdigit() else int(sys.argv[1])
     height = 20 if len(sys.argv) < 3 or not sys.argv[2].isdigit() else int(sys.argv[2])
     m_amount = 25 if len(sys.argv) < 4 or not sys.argv[3].isdigit() else int(sys.argv[3])
-    
+
     board = generate_board(width, height, m_amount)
-    
+
     for i in range(0, len(board)):
         print(str(board[i]) + ' ', end='')
         if (i+1) % width == 0:
